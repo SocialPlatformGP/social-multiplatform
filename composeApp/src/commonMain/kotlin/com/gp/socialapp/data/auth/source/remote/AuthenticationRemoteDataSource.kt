339f92bf09595da1ac187a9f219cf285d9759fb5
Edrees <mohamededrees234@hotmail.com>
2024-03-09 22:04:35 +0200

    added authentication remote source interface

---
+++ composeApp/src/commonMain/kotlin/com/gp/socialapp/data/auth/source/remote/AuthenticationRemoteDataSource.kt
@@ -0,0 +1,13 @@
+package com.gp.socialapp.data.auth.source.remote
+
+import com.gp.socialapp.data.auth.source.remote.model.User
+import com.gp.socialapp.util.Result
+import kotlinx.coroutines.flow.Flow
+
+interface AuthenticationRemoteDataSource {
+    fun signInUser(email: String, password: String): Flow<Result<User>>
+    fun signUpUser(email: String, password: String): Flow<Result<User>>
+    fun getSignedInUser(): User?
+//    fun authenticateWithGoogle(account: GoogleSignInAccount): Flow<State<FirebaseUser>>
+    fun sendPasswordResetEmail(email: String): Flow<Result<Nothing>>
+}
