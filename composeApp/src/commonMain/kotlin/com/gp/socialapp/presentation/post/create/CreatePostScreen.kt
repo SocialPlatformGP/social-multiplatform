d148b9667cb56c409024a52463953261665c8b93
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-03-14 20:34:29 +0200

    refactor: Refactoring the CreatePostScreen.kt to kmp

--- composeApp/src/commonMain/kotlin/com/gp/socialapp/presentation/post/create/CreatePostScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gp/socialapp/presentation/post/create/CreatePostScreen.kt
@@ -1,6 +1,5 @@
 package com.gp.socialapp.presentation.post.create
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -27,19 +26,33 @@ object CreatePostScreen : Screen {
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
         val screenModel = navigator.getNavigatorScreenModel<CreatePostViewModel>()
+        val state by screenModel.uiState.collectAsState()
         MaterialTheme {
-            CreatePostContent(screenModel = screenModel)
+            CreatePostContent(
+                state = state,
+                onBackClick = { navigator.pop() },
+                onPostClick = { screenModel.onCreatePost() },
+                onTitleChange = { screenModel.onTitleChange(it) },
+                onBodyChange = { screenModel.onBodyChange(it) }
+            )
         }
     }
 
-    @OptIn(ExperimentalFoundationApi::class)
     @Composable
     private fun CreatePostContent(
-        screenModel: CreatePostViewModel
+        state: CreatePostUIState,
+        onBackClick: () -> Unit,
+        onPostClick: () -> Unit,
+        onTitleChange: (String) -> Unit,
+        onBodyChange: (String) -> Unit
     ) {
-        val state by screenModel.uiState.collectAsState()
         Scaffold(
-            topBar = { CreatePostTopBar() },
+            topBar = {
+                CreatePostTopBar(
+                    onBackClick = onBackClick,
+                    onPostClick = onPostClick
+                )
+            }
         ) {
             Column(
                 modifier = Modifier
@@ -49,7 +62,7 @@ object CreatePostScreen : Screen {
                 MyTextField(
                     value = state.title,
                     label = "Title",
-                    onValueChange = { screenModel.onTitleChange(it) },
+                    onValueChange = { onTitleChange(it) },
                     modifier = Modifier
                         .fillMaxWidth()
                         .weight(0.2f)
@@ -57,7 +70,7 @@ object CreatePostScreen : Screen {
                 MyTextField(
                     value = state.body,
                     label = "Body",
-                    onValueChange = { screenModel.onBodyChange(it) },
+                    onValueChange = { onBodyChange(it) },
                     modifier = Modifier
                         .fillMaxWidth()
                         .weight(1f)
@@ -67,7 +80,12 @@ object CreatePostScreen : Screen {
                 //TODO: Add files row
                 FilesRow()
                 HorizontalDivider()
-                BottomOptionRow()
+                BottomOptionRow(
+                    onAddFileClicked = { /*TODO: Add file picker*/ },
+                    onAddImageClicked = {/*TODO: Add image picker*/ },
+                    onAddTagClicked = {/*TODO: Add tag picker*/ },
+                    onAddVideoClicked = {/*TODO: Add video picker*/ }
+                )
             }
 
         }
