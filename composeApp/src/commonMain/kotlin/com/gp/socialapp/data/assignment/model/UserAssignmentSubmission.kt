b55f335d282b2c59f56ba64f6438ab678167e579
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-05-05 14:17:38 +0300

    added UserAssignmentSubmission class

---
+++ composeApp/src/commonMain/kotlin/com/gp/socialapp/data/assignment/model/UserAssignmentSubmission.kt
@@ -0,0 +1,17 @@
+package com.gp.socialapp.data.assignment.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserAssignmentSubmission(
+    val id : String = "",
+    val assignmentId: String,
+    val userId: String,
+    val attachments: List<AssignmentAttachment>,
+    val submittedAt: Long = 0,
+    val grade: Int = 0,
+    val isReviewed: Boolean = false,
+    val isAccepted: Boolean = false,
+    val isReturned: Boolean = false,
+    val isTurnedIn: Boolean = false
+    )
