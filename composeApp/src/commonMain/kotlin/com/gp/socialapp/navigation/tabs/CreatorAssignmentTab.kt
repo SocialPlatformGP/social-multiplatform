c1dd88f348c70679e1e0a2c3d29e5fed28512820
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-05-07 11:16:44 +0300

    Edited : Separate Assignment Tab into two tabs user and creator

--- composeApp/src/commonMain/kotlin/com/gp/socialapp/navigation/tabs/CreatorAssignmentTab.kt
+++ composeApp/src/commonMain/kotlin/com/gp/socialapp/navigation/tabs/CreatorAssignmentTab.kt
@@ -1,25 +1,17 @@
 package com.gp.socialapp.navigation.tabs
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Assignment
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import cafe.adriel.voyager.transitions.SlideTransition
 import com.gp.socialapp.presentation.assignment.homeassignment.AssignmentHomeScreen
-import com.gp.socialapp.presentation.home.screen.HomeScreen
 
-object AssignmentsTab : Tab {
+data class CreatorAssignmentTab(val communityId:String) : Tab {
     override val options: TabOptions
         @Composable
         get() {
@@ -28,7 +20,7 @@ object AssignmentsTab : Tab {
 
             return remember {
                 TabOptions(
-                    index =  1u,
+                    index = 2u,
                     title = title,
                     icon = icon
                 )
@@ -38,7 +30,7 @@ object AssignmentsTab : Tab {
     @Composable
     override fun Content() {
         Navigator(
-            screen = AssignmentHomeScreen()
+            screen = AssignmentHomeScreen(communityId)
 //            { onBottomBarVisibilityChanged(it) }
         ) { navigator ->
 //            LaunchedEffect(navigator.lastItem) {
