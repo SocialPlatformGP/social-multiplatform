a2e1c54ed1c269eaf5175b9d8afc009a76c2139b
Edrees <mohamededrees234@hotmail.com>
2024-05-08 17:09:01 +0300

    added DefaultAttachmentPreview

--- composeApp/src/commonMain/kotlin/com/gp/socialapp/presentation/assignment/submissionreview/components/SubmissionAttachmentPreview.kt
+++ composeApp/src/commonMain/kotlin/com/gp/socialapp/presentation/assignment/submissionreview/components/SubmissionAttachmentPreview.kt
@@ -10,22 +10,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BrokenImage
+import androidx.compose.material.icons.filled.Download
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.FilterQuality
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.gp.socialapp.data.assignment.model.AssignmentAttachment
 import com.gp.socialapp.presentation.material.utils.MimeType
@@ -38,25 +39,28 @@ import com.seiko.imageloader.ui.AutoSizeBox
 fun SubmissionAttachmentPreview(
     modifier: Modifier = Modifier,
     attachment: AssignmentAttachment?,
-    onDownloadClicked: (AssignmentAttachment?) -> Unit={}
+    onDownloadClicked: (AssignmentAttachment?) -> Unit = {}
 ) {
-    Column (
+    Column(
         modifier = modifier.fillMaxHeight()
     ) {
-        Row (
+        Row(
             modifier = Modifier.fillMaxWidth()
-        ){
+        ) {
             Text(
                 text = attachment?.name ?: "No attachment selected",
                 style = MaterialTheme.typography.headlineMedium,
                 modifier = Modifier.padding(8.dp)
             )
             Spacer(modifier = Modifier.weight(1f))
-            Button(
-                onClick = {onDownloadClicked(attachment)},
+            IconButton(
+                onClick = { onDownloadClicked(attachment) },
                 modifier = Modifier.padding(8.dp)
-            ){
-                Text("Download")
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Download,
+                    contentDescription = null
+                )
             }
         }
         Box {
@@ -82,7 +86,55 @@ fun SubmissionAttachmentPreview(
                     }
 
                     else -> {
+                        DefaultAttachmentPreview(
+                            attachment = attachment.copy(url = BASE_URL + attachment.url),
+                            onDownloadClicked = onDownloadClicked
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun DefaultAttachmentPreview(
+    modifier: Modifier = Modifier,
+    attachment: AssignmentAttachment,
+    onDownloadClicked: (AssignmentAttachment?) -> Unit = {}
+) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Card(
+            modifier = Modifier.align(Alignment.Center),
+            shape = RoundedCornerShape(24.dp),
+            elevation = CardDefaults.cardElevation(4.dp),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "No preview available for this attachment type.",
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.padding(8.dp)
+                )
+                Spacer(Modifier.size(16.dp))
+                Row (
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    Spacer(Modifier.weight(1f))
+                    Button(
+                        onClick = { onDownloadClicked(attachment) },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Download,
+                            contentDescription = null
+                        )
+                        Spacer(Modifier.size(4.dp))
+                        Text(text = "Download")
                     }
                 }
             }
@@ -94,55 +146,41 @@ fun SubmissionAttachmentPreview(
 private fun ImagePreview(
     modifier: Modifier = Modifier, attachment: AssignmentAttachment
 ) {
-    Box(
-        modifier = modifier.fillMaxSize()
-    ) {
-        Text(
-            text = attachment.name,
-            style = TextStyle(
-                shadow = Shadow(
-                    color = Color.Black,
-                    offset = Offset(5f, 5f),
-                    blurRadius = 5f
+    AutoSizeBox(
+        modifier = modifier.fillMaxSize(),
+        url = attachment.url,
+    ) { action ->
+        when (action) {
+            is ImageAction.Success -> {
+                Image(
+                    painter = rememberImageSuccessPainter(
+                        action = action,
+                        filterQuality = FilterQuality.High
+                    ),
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+                    modifier = modifier
+                        .fillMaxSize()
+                        .align(Alignment.Center)
                 )
-            ),
-            modifier = Modifier.align(Alignment.TopStart).padding(8.dp)
-        )
-        AutoSizeBox(
-            url = attachment.url,
-        ) { action ->
-            when (action) {
-                is ImageAction.Success -> {
-                    Image(
-                        painter = rememberImageSuccessPainter(
-                            action = action,
-                            filterQuality = FilterQuality.High
-                        ),
-                        contentDescription = null,
-                        contentScale = ContentScale.Fit,
-                        modifier = modifier
-                            .fillMaxSize()
-                            .align(Alignment.Center)
-                    )
-                }
+            }
 
-                is ImageAction.Failure -> {
-                    Icon(
-                        imageVector = Icons.Default.BrokenImage,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .align(Alignment.Center)
-                            .size(50.dp),
-                    )
-                }
+            is ImageAction.Failure -> {
+                Icon(
+                    imageVector = Icons.Default.BrokenImage,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(50.dp),
+                )
+            }
 
-                is ImageAction.Loading -> {
-                    CircularProgressIndicator(
-                        modifier = Modifier
-                            .align(Alignment.Center)
-                            .size(50.dp),
-                    )
-                }
+            is ImageAction.Loading -> {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(50.dp),
+                )
             }
         }
     }
