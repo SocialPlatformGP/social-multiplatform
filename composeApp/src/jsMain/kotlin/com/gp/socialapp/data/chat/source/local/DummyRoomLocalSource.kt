a616a51663dbb8c49d5471966cbdfbd63519b223
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-06-14 17:20:09 +0300

    delete web platform

--- composeApp/src/jsMain/kotlin/com/gp/socialapp/data/chat/source/local/DummyRoomLocalSource.kt
+++
@@ -1,6 +0,0 @@
-package com.gp.socialapp.presentation.app.com.gp.socialapp.data.chat.source.local
-
-import com.gp.socialapp.data.chat.source.local.RoomLocalDataSource
-
-object DummyRoomLocalSource: RoomLocalDataSource {
-}
