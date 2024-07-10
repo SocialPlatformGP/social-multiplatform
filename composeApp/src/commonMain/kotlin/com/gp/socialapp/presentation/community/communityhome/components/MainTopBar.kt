f7e7622fb9c4146e26bbdca9cb6af470522f4f79
Edrees <mohamededrees234@hotmail.com>
2024-07-11 02:05:30 +0300

    fixed a ui bug in app top bars where it doesn't consider window insets on mobile

--- composeApp/src/commonMain/kotlin/com/gp/socialapp/presentation/community/communityhome/components/MainTopBar.kt
+++ composeApp/src/commonMain/kotlin/com/gp/socialapp/presentation/community/communityhome/components/MainTopBar.kt
@@ -5,20 +5,24 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.NotificationsActive
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainTopBar(
     onSearchClicked: () -> Unit,
@@ -33,6 +37,7 @@ fun MainTopBar(
                 MaterialTheme.colorScheme.onPrimaryContainer
             )
             .padding(16.dp)
+            .windowInsetsPadding(TopAppBarDefaults.windowInsets)
 
     ) {
         IconButton(onClick = {
